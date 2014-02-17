5a781d2a4541b33781bcb7221d4b25a0c2662921
zhouxing <zhouxing@cd.tuan800.com>
2014-02-17 19:01:14 +0800

    提交gitignore

---
+++ spec/views/messages/index.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "messages/index" do
+  before(:each) do
+    assign(:messages, [
+      stub_model(Message,
+        :user_id => 1,
+        :content => "MyText"
+      ),
+      stub_model(Message,
+        :user_id => 1,
+        :content => "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of messages" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+  end
+end
