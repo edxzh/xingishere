e1c7e8ef75de8a9d52e11c7c1892b287363b603e
zhouxing <zhouxing@cd.tuan800.com>
2014-02-24 22:20:25 +0800

    commit all

--- app/mailers/activate_mailer.rb
+++ app/mailers/activate_mailer.rb
@@ -3,6 +3,7 @@ class ActivateMailer < ActionMailer::Base
   default from: "www.xingishere.com"
 
   def user_activate(user)
+    @user         = user
     @subject      = "帐号激活"
     @to           = user.email
     mail to: @to, subject: @subject
