0e8fbf5282a30ff779d8e7c7ecfac987d18e30c7
zhouxing <zhouxing@cd.tuan800.com>
2014-02-28 10:34:24 +0800

    add

--- db/migrate/20131009153411_create_comments.rb
+++ db/migrate/20131009153411_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
-      t.text :content,    limit: 160, null:false
+      t.text :content,    limit: 160, null: false
       t.integer :blog_id
       t.integer :user_id
       t.integer :pid
