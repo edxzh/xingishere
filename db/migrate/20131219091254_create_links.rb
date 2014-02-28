d267608a2d97ecec292baf6a04e37713d913f310
zhouxing <zhouxing@cd.tuan800.com>
2014-02-28 11:21:53 +0800

    修复migrate文件

--- db/migrate/20131219091254_create_links.rb
+++ db/migrate/20131219091254_create_links.rb
@@ -2,9 +2,9 @@
 class CreateLinks < ActiveRecord::Migration
   def change
     create_table :links do |t|
-      t.string :title,                limit: 20,  null:false
-      t.string :href,                             null:false
-      t.string :category,                         null:false, default:"其它"
+      t.string :title,                limit: 20,  null: false
+      t.string :href,                             null: false
+      t.string :category,                         null: false, default: "其它"
       t.text :description,            limit: 100
 
       t.timestamps
