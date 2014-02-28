e2458c50ab4d08c73896ccf84313273b13029d19
zhouxing <zhouxing@cd.tuan800.com>
2014-02-28 12:54:24 +0800

    migrate bug解决

--- db/migrate/20131009150759_create_users.rb
+++ db/migrate/20131009150759_create_users.rb
@@ -6,16 +6,16 @@ class CreateUsers < ActiveRecord::Migration
       t.string  :name,               limit: 20,    null: false, default: "星哥粉丝"
       t.string  :password_digest
       t.string  :sex
-      t.date    :birthday                          null: false, default: "1970-01-01"
+      t.date    :birthday,                         null: false, default: "1970-01-01"
       t.string  :status
       t.integer :height
       t.string  :position
       t.string  :address
       t.string  :relation
       t.string  :image
-      t.integer :rights                           null: false, default: "2"
+      t.integer :rights,                          null: false, default: "2"
       t.integer :score,                           null: false, default: 0
-      t.text    :description,        limit:100
+      t.text    :description,        limit: 100
       t.timestamps
     end
   end
