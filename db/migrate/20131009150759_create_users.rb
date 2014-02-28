cb3be4c3ee84dffc904805d60013104697634cea
zhouxing <zhouxing@cd.tuan800.com>
2014-02-28 10:34:24 +0800

    add

--- db/migrate/20131009150759_create_users.rb
+++ db/migrate/20131009150759_create_users.rb
@@ -3,18 +3,18 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string  :username,           limit: 20
-      t.string  :name,               limit: 20,    null:false, default:"星哥粉丝"
+      t.string  :name,               limit: 20,    null: false, default:"星哥粉丝"
       t.string  :password_digest
       t.string  :sex
-      t.date    :birthday                          null:false, default:"1970-01-01"
+      t.date    :birthday                          null: false, default:"1970-01-01"
       t.string  :status
       t.integer :height
       t.string  :position
       t.string  :address
       t.string  :relation
       t.string  :image
-      t.integer :rights                           null:false, default:"2"
-      t.integer :score,                           null:false, default:0
+      t.integer :rights                           null: false, default:"2"
+      t.integer :score,                           null: false, default:0
       t.text    :description,        limit:100
       t.timestamps
     end
