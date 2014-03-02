4710458b26ffe3f7ad858b7fd71c1a9a0a38124c
zhouxing <zhouxing@cd.tuan800.com>
2014-03-02 23:08:51 +0800

    application.yml modify

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module Mywebsite
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-      config.autoload_paths += %W(#{config.root}/lib #{/config.root}/app/entries)
+    # %W(#{config.root}/lib #{config.root}/app/entries)
+      config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/entries)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
