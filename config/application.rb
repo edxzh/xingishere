290c628b302216d997ec46775197f7e9c77316b6
zhouxing <zhouxing@cd.tuan800.com>
2014-03-02 23:03:16 +0800

    autoload and assets prefix

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Mywebsite
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+      config.autoload_paths += %W(#{config.root}/lib #{/config.root}/app/entries)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -67,6 +67,7 @@ module Mywebsite
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.prefix = "/assets"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
