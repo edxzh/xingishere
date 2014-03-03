87711364dadd46b9c48636e5205ff6d704b0ea92
zhouxing <zhouxing@cd.tuan800.com>
2014-03-03 20:40:30 +0800

    add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '18edc5c0a4be58a180ec4324dfbcf13785eb242bcd70934a8a91ba0852e20fff3b3c9f99eccc7180d7b7a0af9e7d39ab53bd33690e4bac33d6fb501e5b5dc59c'
