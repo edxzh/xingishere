e80d4fc9e68d7fea31088e988432684e7e485599
star_zhou <Edward_mjz@hotmail.com>
2014-07-30 17:25:46 +0800

    订阅邮件html

--- app/mailers/week_well_chosen_mailer.rb
+++ app/mailers/week_well_chosen_mailer.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 class WeekWellChosenMailer < ActionMailer::Base
   default from: "www.xingishere.com"
+  helper  ApplicationHelper
 
-  def week_blogs(blogs, user)
-    @blogs      = blogs
+  def week_blogs(user)
+    @blogs      = Blog.published.order("view_total ASC, created_at DESC").limit(5)
     @subject    = "撕瓦星哥每周精选"
     @to         = user.email
     mail to: @to, subject: @subject
