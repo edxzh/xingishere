e949f9eee3b4604a68dd425c2c81094ca09d129c
star_zhou <Edward_mjz@hotmail.com>
2014-08-02 22:04:05 +0800

    done part 1

--- app/mailers/week_well_chosen_mailer.rb
+++ app/mailers/week_well_chosen_mailer.rb
@@ -1,12 +1,13 @@
 # encoding: utf-8
 class WeekWellChosenMailer < ActionMailer::Base
   default from: "www.xingishere.com"
+  helper  HomeHelper
   helper  ApplicationHelper
 
-  def week_blogs(user)
+  def week_blogs(email)
     @blogs      = Blog.published.order("view_total ASC, created_at DESC").limit(5)
     @subject    = "撕瓦星哥每周精选"
-    @to         = user.email
+    @to         = email
     mail to: @to, subject: @subject
   end
 end
--- lib/tasks/regular_job.rake
+++ lib/tasks/regular_job.rake
@@ -2,6 +2,9 @@
 namespace :regular_job do
   desc  "每周日晚上21:30给订阅的用户发邮件"
   task  :send_week_chosen => :environment do
-    
+    emails = User.activated.select("email")
+    emails.each do |email|
+      WeekWellChosenMailer.week_blogs(email)
+    end
   end
 end
