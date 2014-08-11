43050f013ef6d2ba98d39b8e7b59ce7064629eca
star_zhou <Edward_mjz@hotmail.com>
2014-08-11 22:05:49 +0800

    week chosen

--- app/mailers/week_well_chosen_mailer.rb
+++ app/mailers/week_well_chosen_mailer.rb
@@ -6,6 +6,12 @@ class WeekWellChosenMailer < ActionMailer::Base
 
   def week_blogs(email)
     @blogs      = Blog.published.order("view_total ASC, created_at DESC").limit(5)
+    category_ids =  BlogCategory.pluck("id")
+    ran_category_ids = category_ids.sample(4)
+    @blog_categories = BlogCategory.find(ran_category_ids)
+    user_ids = User.activated.pluck("id")
+    ran_user_ids = user_ids.sample(4)
+    @users = User.find(ran_user_ids)
     @subject    = "撕瓦星哥每周精选"
     @to         = email
     mail to: @to, subject: @subject
