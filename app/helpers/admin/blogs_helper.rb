f90eaf96c0e41694f6bad86c08715e62d342f558
star_zhou <Edward_mjz@hotmail.com>
2014-11-07 12:05:01 +0800

    done

--- app/helpers/admin/blogs_helper.rb
+++ app/helpers/admin/blogs_helper.rb
@@ -2,9 +2,9 @@
 module Admin::BlogsHelper
   def publish_text(blog)
     if blog.published?
-      "#{link_to('取消发布', admin_blog_path(blog), method: :delete, data: { confirm: '确定取消发布？' })}"
+      "#{link_to('取消发布', toggle_publish_status_admin_blog_path(blog), method: :post, data: { confirm: '确定取消发布？' })}"
     else
-      "#{link_to('发布', admin_blog_path(blog), method: :delete, data: { confirm: '确定发布？' })}"
+      "#{link_to('发布', toggle_publish_status_admin_blog_path(blog), method: :post, data: { confirm: '确定发布？' })}"
     end
   end
 end
