bbb2567befead4244efff42de82337ee3ea95eda
star_zhou <Edward_mjz@hotmail.com>
2014-11-28 17:24:41 +0800

    xingge.html and unicorn.rb

--- config/unicorn_init.rb
+++ config/unicorn_init.rb
@@ -1,9 +1,29 @@
-<div class="footer">
-  <div class="f_content">
-    <p>基于rails + mysql + mongo + nginx构建</p>
-    <p>版权所有©2013 xingishere.com</p>
-    <p>host on digitalocean</p>
-    <p>域名购买于Godaddy</p>
-    <p>DNSPOD解析</p>
-  </div>
-</div>
+# -*- encoding : utf-8 -*-
+worker_processes 3
+timeout 30
+listen File.expand_path("/tmp/My_blog.sock"), :backlog => 64
+preload_app true
+
+pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = File.expand_path('../../Gemfile', __FILE__)
+end
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
