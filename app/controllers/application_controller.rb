f4d1e3630d9dc244be916aee2ba6ff6d2f244a24
star_zhou <Edward_mjz@hotmail.com>
2014-11-29 11:05:48 +0800

    static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   def is_admin
     user = User.find(session[:user_id]) if session[:user_id].present?
     if !user || !user.admin?
-      redirect_to '/404.html'
+      render_404_page
     end
   end
 
