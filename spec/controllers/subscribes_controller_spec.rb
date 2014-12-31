070f55e9c592c47bdbcf21052ae446076510463c
Dogzhou <Edward_mjz@hotmail.com>
2014-12-31 18:57:56 +0800

    controller subscribe

---
+++ spec/controllers/subscribes_controller_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe SubscribesController do
+
+  describe "create" do
+    before(:each) do
+      @subscribe = FactoryGirl.create(:subscribe)
+    end
+
+    it "create success" do
+      post :create, email: "Edward_mjz@hotmail.com"
+      expect(response.headers['Content-Type']).to have_content("application/json")
+      expect(response.body).to eq({ "status" => 1, "message" => "订阅成功" }.to_json)
+    end
+
+    it "create fail" do
+    end
+
+  end
+end
