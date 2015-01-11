56a80153b9d2fb46c0a9c066a0751f5ffd070ba0
zhouxing <Edward_mjz@hotmail.com>
2015-01-11 12:08:09 +0800

    smtp with settings

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,11 +3,11 @@ Mywebsite::Application.configure do
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       :authentication => :plain,
-      :address        => "smtp.mailgun.org",
-      :port           => 587,
-      :domain         => "www.xingishere.com",
-      :user_name      => "postmaster@xingishere.com",
-      :password       => Settings.smtp_password,
+      :address        => Setting.smtp.address,
+      :port           => Setting.smtp.port,
+      :domain         => Settings.smtp.domain,
+      :user_name      => Settings.smtp.user_name,
+      :password       => Settings.smtp.password,
     }
   end
 end
