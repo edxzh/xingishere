99fc8d392f4f33098edaad320fffc1dce546df0e
zhouxing <Edward_mjz@hotmail.com>
2015-01-17 11:21:02 +0800

    aside and api

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,10 +15,13 @@ module ApplicationHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user, options = { size: 50 })
-    gravatar_id   = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
+    image_tag(avatar_url(user, size), alt: user.name, class: "gravatar")
+  end
+
+  def avatar_url(user, size=50)
+    gravatar_id   = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url  = "http://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
   def format_time(time)
--- app/views/api/blogs/show.json.jbuilder
+++ app/views/api/blogs/show.json.jbuilder
@@ -1,6 +1,11 @@
 json.title @blog.title
 json.content @blog.content
-json.created_at @blog.created_at
+json.category_name @blog.blog_category_name
+json.created_at @blog.created_at.to_s(:db)
+json.comments_count @blog.comments.count
 json.comments @blog.comments do |comment|
-  json.avatar gravatar_for comment.user
+  json.avatar avatar_url(comment.user, params[:avatar_size] || 50)
+  json.name comment.name
+  json.content h(comment.content)
+  json.created_at comment.created_at.to_s(:db)
 end
