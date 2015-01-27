c2d62fccc4009f7cee2b0cb32f88d4c5db74f2a6
zhouxing <Edward_mjz@hotmail.com>
2015-01-27 21:11:56 +0800

    db

--- db/migrate/20131009153248_create_blogs.rb
+++ db/migrate/20131009153248_create_blogs.rb
@@ -3,7 +3,7 @@ class CreateBlogs < ActiveRecord::Migration
   def change
     create_table :blogs, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', comment: '博客表' do |t|
       t.string :title,              limit: 30,   null: false,  default: "我的日志",  comment: "标题"
-      t.text :content,                           null: false,                        comment: "内容"
+      t.text :content,                           null: false,  default: "",          comment: "内容"
       t.integer :user_id,                        null: false,  default: 0,           comment: "用户ID"
       t.string :tag,                                                                 comment: "标签"
       t.integer :blog_category_id,               null: false,                        comment: "博客类型ID"
