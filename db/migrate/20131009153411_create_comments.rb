25f5977964ce4f728234ad538971391098f1a93e
zhouxing <Edward_mjz@hotmail.com>
2015-05-30 16:03:45 +0800

    add field nickname and email to comments table

--- db/migrate/20131009153411_create_comments.rb
+++ db/migrate/20131009153411_create_comments.rb
@@ -7,8 +7,10 @@ class CreateComments < ActiveRecord::Migration
       t.integer :user_id,             null: false,  default: 0,     comment: "用户ID"
       t.integer :pid,                                               comment: "回复的评论ID"
       t.boolean :publish_status,      null: false,  default: true,  comment: "发布状态"
-      t.string  :remote_ip,           null: false,  default: '',    comment: "评论者IP"
-      t.string  :address,             null: false,  default: '',    comment: "评论者所在省市"
+      t.string  :remote_ip,           limit: 20,    null: false,  default: '',    comment: "评论者IP"
+      t.string  :address,             limit: 100,   null: false,  default: '',    comment: "评论者所在省市"
+      t.string  :nickname,            limit: 20,    null: false,  default: '',    comment: "昵称"
+      t.string  :email,               limit: 50,    null: false,  default: '',    comment: "电子邮箱"
       t.timestamps
     end
     add_index :comments,  :user_id
