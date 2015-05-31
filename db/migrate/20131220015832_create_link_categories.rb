89b80286627e6e21722aa486c981ac05d2493f47
zhouxing <Edward_mjz@hotmail.com>
2015-05-31 17:16:38 +0800

    user_id in link_categories can be null

--- db/migrate/20131220015832_create_link_categories.rb
+++ db/migrate/20131220015832_create_link_categories.rb
@@ -3,7 +3,7 @@ class CreateLinkCategories < ActiveRecord::Migration
   def change
     create_table :link_categories, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', comment: '友链类型表' do |t|
       t.string    :category,   limit: 100,  null: false,      comment: '类型名'
-      t.integer   :user_id,                 null: false,      comment: 'user_id'
+      t.integer   :user_id,                                   comment: 'user_id'
       t.timestamps
     end
   end
