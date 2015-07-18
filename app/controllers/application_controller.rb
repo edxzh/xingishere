c2157bd944c591b8e6ba1b06adfc028be5e456d9
zhouxing <Edward_mjz@hotmail.com>
2015-07-18 22:00:32 +0800

    set varnish cache header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 # encoding : utf-8
 class ApplicationController < ActionController::Base
   # before_filter :authorize
+  before_filter :set_cache_control_headers, if: :is_prod?
   protect_from_forgery with: :exception
+
   include SessionsHelper
+
   def authorize
     if User.find_by_id(session[:user_id]).blank?
       redirect_to user_login_path, alert: "请您先登录"
@@ -29,6 +32,14 @@ class ApplicationController < ActionController::Base
       format.xml { render :xml => { :error => 'Not found' }, :status => 404 }
       format.html { render template: 'errors/404', status: 404, layout: 'static' }
     end
-    # redirect_to "/404.html"
   end
-end
+
+  def is_prod?
+    Rails.env.production?
+  end
+
+  def set_cache_control_headers(max_age = 5.minutes.to_s)
+    response.headers['Cache-Control'] = 'public, no-cache'
+    response.headers['Surrogate-Control'] = "max-age=#{max_age}"
+  end
+  end
