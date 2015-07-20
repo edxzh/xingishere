4311bc6adee2162564600ac75d812e8c8434ed07
zhouxing <Edward_mjz@hotmail.com>
2015-07-20 23:42:52 +0800

    homepage background image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  layout 'home'
   skip_before_filter  :authorize
   def index
     @blogs     = Blog.published.includes(:blog_category, :tags).weight_order.page(params[:page]).per(10)
