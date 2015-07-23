9f8d7dac5ec94c3ca49ab229751f5e2357fb9448
zhouxing <Edward_mjz@hotmail.com>
2015-07-23 23:59:40 +0800

    add paginate and footer to home page

--- app/models/subscribe.rb
+++ app/models/subscribe.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 class Subscribe < ActiveRecord::Base
   attr_accessible :email
-  validates :email, presence: { message: "请输入email" }, uniqueness: { message: "您已经订阅" }, format: { with: Regexp.new(Settings.regexp.email), message:'邮件格式不正确' }
+  validates :email, presence: { message: "please input email" }, uniqueness: { message: "you have subscribed" }, format: { with: Regexp.new(Settings.regexp.email), message:'email format incorrect' }
 end
