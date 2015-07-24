98c80e806facb69c927353eebd9d3d6316c3ffe3
zhouxing <Edward_mjz@hotmail.com>
2015-07-24 22:49:43 +0800

    comment the unuseful test

--- spec/controllers/subscribes_controller_spec.rb
+++ spec/controllers/subscribes_controller_spec.rb
@@ -11,12 +11,12 @@ describe SubscribesController do
     it "create success" do
       post :create, email: "Edward_mjz@hotmail.com"
       expect(response.headers['Content-Type']).to have_content("application/json")
-      expect(response.body).to eq({ "status" => 1, "message" => "订阅成功" }.to_json)
+      expect(response.body).to eq({ "status" => 1, "message" => "subscribe success" }.to_json)
     end
 
     it "create fail" do
       post :create, email: "example@qq.com"
-      expect(response.body).to eq({ "status" => -1, "message" => "您已经订阅" }.to_json)
+      expect(response.body).to eq({ "status" => -1, "message" => "you have subscribed" }.to_json)
     end
 
   end
