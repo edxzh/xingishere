6b889af685869bd9aab8e3fc431c09d9e347dc5f
zhouxing <Edward_mjz@hotmail.com>
2015-07-26 12:09:00 +0800

    change 5 blogs per page to 10 blogs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   layout 'home'
   skip_before_filter  :authorize
   def index
-    @blogs     = Blog.published.includes(:blog_category, :tags).weight_order.page(params[:page]).per(5)
+    @blogs     = Blog.published.includes(:blog_category, :tags).weight_order.page(params[:page]).per(10)
   end
 end
