8d7396cc6f3497d9ea726ea4210414e1542cb5d9
zhouxing <Edward_mjz@hotmail.com>
2015-09-13 15:43:32 +0800

    change model validation format regExp to the way that rails 4 recommend. :grin:

--- app/models/subscribe.rb
+++ app/models/subscribe.rb
@@ -2,5 +2,5 @@
 class Subscribe < ActiveRecord::Base
   validates :email, presence: { message: 'please input email' },
     uniqueness: { message: 'you have subscribed' },
-    format: { with: Regexp.new(Settings.regexp.email), message: 'email format incorrect' }
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'email format incorrect' }
 end
