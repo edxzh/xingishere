9b265ff41b98a4498a0ec4c62e6c5ae9053a07d9
Dogzhou <Edward_mjz@hotmail.com>
2015-09-30 12:53:42 +0800

    Add CKEditor to assets precompile. :kissing_heart:

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile = [ Proc.new{ |path| !File.extname(path).in?(['.html', '.htm']) } ]
 Rails.application.config.assets.paths.unshift Rails.root.join("app", "assets").to_s
+Rails.application.config.assets.precompile += %w( ckeditor/* )
