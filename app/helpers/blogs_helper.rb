0dc2375a6308084d65fa97c9eac5a384c1ba07e3
Dogzhou <Edward_mjz@hotmail.com>
2015-11-03 17:32:15 +0800

    Add home helper rspec. :neutral_face:

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,4 +1,3 @@
-# encoding : utf-8
 module BlogsHelper
   def like_icon
     if @like
--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe HomeHelper do
-  before(:each) do
-    @blog = FactoryGirl.create(:top_blog)
+  let(:top_blog) { FactoryGirl.create(:top_blog) }
+
+  describe '.weight_status' do
+    it 'should return correct html with 顶' do
+      expect(weight_status(top_blog)).to eq "<span class='ding'>[顶]</span>"
+    end
   end
 
-  it 'weight_status' do
-    expect(weight_status(@blog)).to eq "<span class='ding'>[顶]</span>"
+  describe '.display_content' do
+    let(:random_string) { SecureRandom.hex(150) }
+
+    it 'should return first 240 with ......' do
+      expect(display_content(random_string)).to eq random_string[0...240] + '......'
+    end
   end
 end
