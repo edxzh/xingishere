7f6e7d953f439b23aba0b87a4bdda0e6da71f0db
zhouxing <Edward_mjz@hotmail.com>
2015-12-18 21:46:18 +0800

    Remove keyword search function.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,8 +2,6 @@ class BlogsController < ApplicationController
   def index
     if params[:category].present?
       relation = Blog.published.weight_order.includes(:comments).category(params[:category])
-    elsif params[:keyword]
-      relation = Blog.published.weight_order.includes(:comments).keyword(params[:keyword])
     else
       relation = Blog.published.weight_order.includes(:blog_category, :comments)
     end
