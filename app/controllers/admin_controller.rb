9f839dfd1fdca83e352cf38c8d4de18029400c5f
zhouxing <Edward_mjz@hotmail.com>
2015-12-19 10:02:25 +0800

    Refactor admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,18 +1,10 @@
 class AdminController < ApplicationController
   before_action :is_admin
 
-  def user_login
-    if session[:user_id].blank?
-      store_location
-      flash[:info] = '请您先登录'
-      redirect_to login_path
-    end
-  end
-
   def is_admin
     user = User.find(session[:user_id]) if session[:user_id].present?
     if !user || !user.admin?
-      redirect_to '/404.html'
+      render_404_page
     end
   end
 end
